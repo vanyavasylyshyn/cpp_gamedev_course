0ec27b5348d658c125229bbc073e27af2c508c8c
Ivan Vasylyshyn <ivan.vasylyshyn@digit.az>
2024-11-29 15:52:45 +0200

    Finish homework

--- lesson_1/main.cpp
+++ lesson_1/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
+void build_triangle(int rows_num);
+
 int main() {
     std::cout << "I love C++!" << std::endl;
-    return 0;
+
+    build_triangle(10);
+}
+
+void build_triangle(const int rows_num) {
+    for(int i = 0; i < rows_num; i++) {
+        std::cout << std::string(i, '*') << std::endl;
+    }
 }
